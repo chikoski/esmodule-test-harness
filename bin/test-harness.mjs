9355eb1c7287144c257d1b261bfe590be4249aa6
N.Shimizu <chikoski@gmail.com>
2019-01-25 20:42:23 +0900

    enabled to handle multi-layered description and other declarations, such as it, before, after

--- bin/test-harness.mjs
+++ bin/test-harness.mjs
@@ -1,38 +1,18 @@
 #!/usr/bin/env node --experimental-modules
 
-class Runtime {
-  constructor(harness) {
-    this.harness = harness.create();
-  }
-  start(entryPoint) {
-    import(entryPoint)
-      .then(result => this.run())
-      .catch(e => this.loadError(e));
-  }
-  run() {
-    this.harness.start();
-    console.log(this.harness.lastSession + "")
-  }
-  loadError(e) {
-    console.log(e + "");
-  }
-}
+import { Runtime } from "../lib/runtime.mjs"
+import { Harness } from "../lib/harness.mjs"
+import path from "path"
 
 const DEFAULT = "./test/index.mjs";
-
-Promise.all([
-  import("../index.mjs"),
-  import("path")
-]).then(([Harness, path]) => {
-  function expandPath(fragment) {
-    if (fragment.charAt(0) === "/") {
-      return path;
-    }
-    return path.join(process.cwd(), fragment);
+function expandPath(fragment) {
+  if (fragment.charAt(0) === "/") {
+    return path;
   }
+  return path.join(process.cwd(), fragment);
+}
 
-  const args = process.argv;
-  const entryPoint = args.length > 2 ? args[2] : DEFAULT;
-  const runtime = new Runtime(Harness.Harness);
-  runtime.start(expandPath(entryPoint));
-});
+const args = process.argv;
+const entryPoint = args.length > 2 ? args[2] : DEFAULT;
+const runtime = new Runtime(Harness);
+runtime.start(expandPath(entryPoint));
--- index.mjs
+++ index.mjs
@@ -1,139 +1,40 @@
-class Testcase {
-  constructor(description, body) {
-    this.mDescription = description;
-    this.mBody = body;
-  }
-  get description() {
-    return this.mDescription;
-  }
-  get body() {
-    return this.mBody;
-  }
-}
-
-class Trial {
-  constructor(testCase) {
-    this.testCase = testCase;
-    this.error = null;
-  }
-  get description() {
-    return this.testCase.description;
-  }
-  isFailed() {
-    return this.error != null;
-  }
-  run() {
-    try {
-      this.testCase.body();
-    } catch (error) {
-      this.error = error;
-    }
-  }
-}
-
-class Stats {
-  constructor(result) {
-    this.passed = result.passed.length;
-    this.failed = result.failed.length;
-  }
-  get total() {
-    return this.passed + this.failed;
-  }
-  get coverage() {
-    const actual = this.passed / this.total;
-    return Math.floor(actual * 10000) / 100;
-  }
-  toString() {
-    return `Total ${this.total} / ${this.passed} passed / ${this.failed} failed / ${this.coverage}% coverage`;
-  }
-}
+import { Harness } from "./lib/harness.mjs"
 
 const formatter = new Map([
   ["ERR_ASSERTION", error => `assersion error (${error.actual} ${error.operator} ${error.expected})`],
   ["unknown", error => error + ""]
 ]);
 
-class Result {
-  constructor(session) {
-    this.passed = session.done.filter(i => !i.isFailed());
-    this.failed = session.failed;
-    this.stats = new Stats(this);
-  }
-  toString() {
-    let result = this.stats.toString() + "\n";
-    result += this.failed.map(fail => {
-      const code = fail.error.code;
-      const func = formatter.has(code) ? formatter.get(code) : formatter.get("unknown");
-      return `${fail.description} : ${func(fail.error)}`;
-    }).join("\n");
-    return result;
-  }
-}
-
-class Session {
-  constructor(list) {
-    this.all = list;
-    this.done = [];
-    this.failed = [];
-  }
-  get finished() {
-    return this.all.length === this.done.length;
-  }
-  get result() {
-    if (!this.finished) {
-      return null;
-    }
-    return new Result(this);
-  }
-  start() {
-    this.run();
-  }
-  run() {
-    for (const tc of this.all) {
-      const trial = new Trial(tc);
-      trial.run();
-      if (trial.isFailed()) {
-        this.failed.push(trial);
-      }
-      this.done.push(trial);
-    }
-  }
-}
-
-export class Harness {
-  constructor() {
-    this.mAll = [];
-    this.mResult = null;
-  }
-  get all() {
-    return this.mAll;
-  }
-  get lastSession() {
-    return this.mResult;
-  }
-  register(description, body) {
-    this.mAll.push(new Testcase(description, body));
-  }
-  start() {
-    const session = new Session(this.all);
-    this.run(session);
-  }
-  run(session) {
-    if (session == null) {
-      this.start();
-    }
-    session.start();
-    this.mResult = session.result;
-  }
-}
-
-Harness._instance = new Harness();
-Harness.create = function () {
-  return this._instance;
-};
-
 export function describe(description, body) {
-  Harness.create().register(description, body);
+  Harness.create().describe(description, body);
 }
 
-export default { Harness, describe };
+export function it(description, body) {
+  Harness.create().testcase(description, body);
+}
+
+export function before(func) {
+  Harness.create().before(func);
+}
+
+export function after(func) {
+  Harness.create().after(func);
+}
+
+export function beforeEach(func) {
+  Harness.create().beforeEach(func);
+}
+
+export function afterEach(func) {
+  Harness.create().afterEach(func);
+}
+
+global.describe = describe;
+global.it = it;
+global.before = before;
+global.after = after;
+global.beforeEach = beforeEach;
+global.afterEach = afterEach;
+global.Harness = Harness;
+
+export default { describe, it, before, after, beforeEach, afterEach };
